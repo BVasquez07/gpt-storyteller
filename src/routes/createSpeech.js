51241aa82e912627a924c59b3ce765ea4ab7c4df
BVasquez07 <bvasq07@gmail.com>
2024-01-15 23:54:32 -0500

    minor changes to text input

--- src/routes/createSpeech.js
+++ src/routes/createSpeech.js
@@ -2,25 +2,29 @@ const fs = require('fs');
 const txtToSpeech = require('@google-cloud/text-to-speech');
 require('dotenv').config();
 const projectId = process.env.PROJ_ID;
-
-const client = new txtToSpeech.TextToSpeechClient({ projectId })
+const client = new txtToSpeech.TextToSpeechClient({ projectId });
+const express = require("express");
+const router = express.Router();
 
 // TODO: make it so that when the file is created it is immediately played.
 
 const speak = async () => {
-    const text = "Hello, my name is Brandon Vasquez! It's a pleasure to meet you."
+    const text = "Hello, my name is Brandon it's a pleasure to meet you. May I know your name stranger?"
     const speechReqBody = {
         input: {
             text: text
         },
         voice: {
-            languageCode: 'en-US', ssmlGender: 'NEUTRAL'
+            languageCode: 'en-US', 
+            name: "en-US-Journey-F",
+            ssmlGender: "FEMALE",
+            naturalSampleRateHertz: 24000
         },
         audioConfig: {
             audioEncoding: 'MP3',
             effectsProfileId: ["small-bluetooth-speaker-class-device"],
             pitch: 0,
-            speakingRate: 1.13}, 
+            speakingRate: 1.05}, 
     }
     client.synthesizeSpeech
     const [response] = await client.synthesizeSpeech(speechReqBody);
